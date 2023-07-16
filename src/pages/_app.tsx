62bef719d1e909f92f5fd5b52cb4a9b3d29f6e8e
Anirudha Pradhan <anirudhapradhan403@gmail.com>
2023-07-16 07:23:26 +0530

    react-char-add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,8 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import Head from "next/head";
-import { title } from "process";
 import { SideBar } from "~/components/SideBar";
+import Chatbot from "~/components/Chatbot";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -19,19 +19,23 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <meta name="description" content="next js full stack web Badsha Laskar" />
         <link rel="icon" href="" />
       </Head>
-      <div className="sm:pr-4 container text-gray-200
-       mx-auto max-w-full flex 
-       bg-gradient-to-r
-       from-gray-700
-       via-gray-900
-       to-black">
+      <div className="sm:pr-4 
+      container
+      mx-auto max-w-full flex 
+      bg-gradient-to-r
+      text-gray-200
+      from-gray-700
+      via-gray-900
+      to-black">
         <SideBar />
         <div className="
         flex-grow min-h-screen">
           <Component {...pageProps} />
         </div>
-        <div className="hidden md:min-w-[20%] md:block">
-          space
+        <div className="hidden
+        
+        md:block">
+          <Chatbot/>
         </div>
       </div>
     </SessionProvider>
